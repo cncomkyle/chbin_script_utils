eb0bcc0ad908133c318fa92713e6b3aac5f77e0f
chbin <chbin@cisco.com>
2016-02-01 13:26:47 +0800

    change \e to \033

--- export_vars.sh
+++ export_vars.sh
@@ -1,4 +1,4 @@
-export PS1="[\$(date +%Y_%m_%d#%H:%M:%S)]\[\e[31;1m\h@\[\e[0m\][\w]\n$> "
+export PS1="[\$(date +%Y_%m_%d#%H:%M:%S)]\[\e[31;1m\h@\033[0m[\w]\n$> "
 
 export CLICOLOR=1
 export LSCOLORS=edfccbdxbeegedabagacad
